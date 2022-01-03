5a4cb1f093c93181b144c0ecc0eee2b8ec0be61a
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-03 21:11:46 +0800

    small fix for movie.py and add tests in test_movie.py.

--- movie.py
+++ movie.py
@@ -7,7 +7,7 @@
 class Movie:
     """..."""
 
-    def __init__(self, title="", category="", year=0, is_watched=False):
+    def __init__(self, title="", year=0, category="", is_watched=False):
         self.is_watched = True
         self.title = title
         self.category = category
@@ -15,12 +15,14 @@ class Movie:
         self.is_watched = is_watched
 
     def not_watch(self):
-        if self.is_watched is False:
-            self.is_watched = 'Unwatch'
+        if not self.is_watched:
+            self.is_watched = "Unwatch"
+            return self.is_watched
 
     def watched(self):
-        if self.is_watched is True:
-            self.is_watched = 'Watched'
+        if self.is_watched:
+            self.is_watched = "Watched"
+            return self.is_watched
 
     def __str__(self):
         return f"Title: {self.title} Category: {self.category} Year: {self.year} Watched: {self.is_watched}"
--- test_movie.py
+++ test_movie.py
@@ -15,11 +15,24 @@ def run_tests():
     assert not default_movie.is_watched
 
     # Test initial-value movie
+    print("Initial movie:")
     initial_movie = Movie("Thor: Ragnarok", 2017, "Comedy", True)
     print(initial_movie)
     # TODO: Write tests to show this initialisation works
-
+    print("Movie two:")
+    movie_two = Movie("Star war", 1997, "Action", False)
+    print(movie_two)
     # TODO: Add more tests, as appropriate, for each method
+    print("Movie three:")
+    movie_three = Movie("The fugitive", 1993, "Drama", True)
+    print(movie_three)
 
+    print("Movie four:")
+    movie_four = Movie()
+    movie_four.title = "LALALA"
+    movie_four.year = 2022
+    movie_four.category = "Action"
+    movie_four.is_watched = True
+    print(movie_four)
 
 run_tests()
