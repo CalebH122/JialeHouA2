82cc87aa4e779cb06f33570330a915e10aaeca7a
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-03 20:59:03 +0800

    add not_watch and watched function according to assignment requirement.

--- movie.py
+++ movie.py
@@ -7,20 +7,20 @@
 class Movie:
     """..."""
 
-    def __init__(self, title="", category="", year=0, is_watched=""):
+    def __init__(self, title="", category="", year=0, is_watched=False):
         self.is_watched = True
         self.title = title
         self.category = category
         self.year = year
-        self.is_watch = is_watched
+        self.is_watched = is_watched
 
-    def is_watched(self):
-        if not self.is_watch:
-            self.is_watched = 'u'
-            return self.is_watched
-        else:
-            self.is_watched = 'w'
-            return self.is_watched
+    def not_watch(self):
+        if self.is_watched is False:
+            self.is_watched = 'Unwatch'
+
+    def watched(self):
+        if self.is_watched is True:
+            self.is_watched = 'Watched'
 
     def __str__(self):
         return f"Title: {self.title} Category: {self.category} Year: {self.year} Watched: {self.is_watched}"
