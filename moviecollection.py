505c750110aa3f4bf6423d4dd1867d71a7cdb744
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-04 20:46:35 +0800

    Add init and str in moviecollection.py to test empty list.

--- moviecollection.py
+++ moviecollection.py
@@ -2,8 +2,20 @@
 
 
 # TODO: Create your MovieCollection class in this file
+from movie import Movie
 
 
-class MovieCollection:
+class MovieCollection(Movie):
     """..."""
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.movies = []
+
+    def __str__(self):
+        if not self.movies:
+            print("No movie")
+
+
+
+
