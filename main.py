1b10fa8ea6c96a2d20a06fd2b7e2bc222fa1821e
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-08 14:57:05 +0800

    Add load_movies and basic structure code in kv file.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.app import App
 from movie import Movie
 from moviecollection import MovieCollection
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.uix.button import Button
 
 
 class MoviesToWatchApp(App):
@@ -18,13 +18,29 @@ class MoviesToWatchApp(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.status = "Movie to watch 2.0"
         self.movies_to_watch = MovieCollection()
         self.movies_to_watch.load_movies('movies.csv')
 
     def build(self):
         self.title = "Movie to watch 2.0"
         self.root = Builder.load_file('app.kv')
+        self.load_movies()
         return self.root
+
+    def load_movies(self):
+        for i, movie in enumerate(self.movies_to_watch.movies):
+            movie_button = Button()
+            movie_button.id = i
+            if movie.is_watched:
+                movie.is_watched = 'Watched'
+                movie_button.background_color = (0.8, 1, 0, 1)
+            else:
+                movie.is_watched = ''
+                movie_button.background_color = (0.1, 0.8, 0.8, 1)
+            movie_button.text = f'{movie.title} ({movie.category} from {movie.year}) {movie.is_watched}'
+            self.root.ids.movie_list.add_widget(movie_button)
+
 
 if __name__ == '__main__':
     MoviesToWatchApp().run()
